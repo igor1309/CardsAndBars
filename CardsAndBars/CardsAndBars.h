3b4a90ff71b2fe515b6ab2ab6f83d65630f72bdf
Igor Malyarov <photo.igor@mac.com>
2019-08-14 09:40:35 +0300

    Initial Commit

---
+++ CardsAndBars/CardsAndBars.h
@@ -0,0 +1,19 @@
+//
+//  CardsAndBars.h
+//  CardsAndBars
+//
+//  Created by Igor Malyarov on 14.08.2019.
+//  Copyright © 2019 Igor Malyarov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardsAndBars.
+FOUNDATION_EXPORT double CardsAndBarsVersionNumber;
+
+//! Project version string for CardsAndBars.
+FOUNDATION_EXPORT const unsigned char CardsAndBarsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardsAndBars/PublicHeader.h>
+
+
